7331b1152179d82870d8f86519c4f1035145ec20
Abdul Majid <abdulmajidcse@gmail.com>
2024-02-03 14:49:18 +0600

    Handle input error with try-catch.

--- FirstSwingApp.java
+++ FirstSwingApp.java
@@ -2,8 +2,25 @@ import javax.swing.JOptionPane;;
 
 public class FirstSwingApp {
     public static void main(String[] args) {
-        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter First Number:"));
-        int num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter Second Number:"));
+        int num1, num2;
+
+        while (true) {
+            try {
+                num1 = Integer.parseInt(JOptionPane.showInputDialog("Enter First Number:"));
+                break;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Please, enter an integer number!");
+            }
+        }
+
+        while (true) {
+            try {
+                num2 = Integer.parseInt(JOptionPane.showInputDialog("Enter Second Number:"));
+                break;
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Please, enter an integer number!");
+            }
+        }
 
         JOptionPane.showMessageDialog(null, "Sum: " + (num1 + num2));
     }
